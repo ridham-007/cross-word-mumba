9923011233c4915d157cf1e67a109f4a08f9dc17
Ridham Avaiya <77951881+ridham-007@users.noreply.github.com>
2025-05-20 15:48:46 +0530

    Fix Webpack caching issue

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
